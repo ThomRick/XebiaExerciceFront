e777c96363a8196b24eded4cc7fdda5c66b6f0c4
Thomas RICART <Thomas RICART>
2016-08-03 23:56:46 +0200

    Ajout de la fonctionnalité de commande

--- src/main/app/commande/controllers/CommandeController.ts
+++ src/main/app/commande/controllers/CommandeController.ts
@@ -1,19 +1,40 @@
 import IPanierService from "../../panier/services/IPanierService";
+import IOfferService from "../services/IOfferService";
+import IBookService from "../../xebia/services/IBookService";
+import Book from "../../xebia/models/Book";
+import Offer from "../../xebia/models/Offer";
+import CommercialOffer from "../../xebia/models/CommercialOffer";
 
 /**
  * CommandeController
  */
 class CommandeController {
 
-    static panierService: IPanierService;
-
     static $inject = [
-        "panierService"
-    ]
+        "panierService",
+        "offerService",
+        "bookService"
+    ];
 
-    constructor(panierService: IPanierService) {
-        CommandeController.panierService = panierService;
-        console.log(CommandeController.panierService.getBooks());        
+    private books: Array<Book> = [];
+    private amount: number = 0;
+    private offer: Offer = {
+        type: "",
+        value: 0   
+    };
+    private offerAmount: number = 0;
+
+    constructor(panierService: IPanierService, offerService: IOfferService, bookService: IBookService) {
+        this.books = panierService.getBooks();
+        this.amount = panierService.getAmount();
+        let self = this;
+        console.log(this.books);
+        
+        bookService.getCommercialOffers(this.books)
+        .then((commercialOffer: CommercialOffer) => {
+            self.offer = offerService.getBestOffer(self.amount, commercialOffer);
+            self.offerAmount = offerService.computeOfferAmount(self.offer, self.amount);
+        });
     }
 }
 export default CommandeController;
